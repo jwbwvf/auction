9ac2458adba908fea3bf81e6c9793cec08373a26
jason brady <2593225+jwbwvf@users.noreply.github.com>
2017-10-12 21:38:55 -0500

    initial commit

---
+++ app/components/footer/footer.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'auction-footer',
+    templateUrl: 'app/components/footer/footer.html'
+})
+
+export default class FooterComponent {
+    
+}
