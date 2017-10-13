a292981c5cdbac49388b2d7e8150d5cd32e82ac2
jason brady <2593225+jwbwvf@users.noreply.github.com>
2017-10-12 21:45:27 -0500

    initial commit

---
+++ app/components/stars/stars.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  templateUrl: 'app/components/stars/stars.html',
+  selector: 'auction-stars',
+  inputs: ['rating', 'count']
+})
+
+export default class StarsComponent implements OnInit {
+  count: number = 5;
+  rating: number = 0;
+  stars: boolean[] = [];
+
+  ngOnInit() {
+    for (let i = 1; i <= this.count; i++) {
+      this.stars.push(i > this.rating);
+    }
+  }
+}
